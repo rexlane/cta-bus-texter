7bdbbf25a38bac293ea69696cece78dbddaac3eb
Rex Lane <rex.m.lane@gmail.com>
2015-12-09 21:14:15 -0600

    Hooray

--- cta-bus-texter.rb
+++ cta-bus-texter.rb
@@ -8,21 +8,28 @@ Dotenv.load
 
 account_sid = ENV['ACCOUNT_SID']
 auth_token = ENV['AUTH_TOKEN']
-client = Twilio::REST::Client.new account_sid, auth_token
 
+@client = Twilio::REST::Client.new account_sid, auth_token
 
-@body = params[:Body]
-
-
-get '/' do
-#   'Sinatra is running on Heroku. Currently running version ' + Twilio::VERSION + \
-#         ' of the twilio-ruby library.'
+# @client.messages.list.each do |message|
+#   puts message.direction
+#   puts message.body
 # end
 
-# get '/sms-quickstart' do
+
+# @message = @client.messages.create(
+#   to: ENV['MY_NUM'],
+#   from: ENV['TWILIO_NUM'],
+#   body: "This is a test."
+#   media_url: "http://twilio.com/heart.jpg"
+# )
+
+get '/' do
+  @incoming_body = params[:Body]
+
   twiml = Twilio::TwiML::Response.new do |r|
     r.Message do |message| 
-      message.Body "The thing you said was #{@body}"
+      message.Body "The thing you said was #{@incoming_body}"
       # message.Media "https://demo.twilio.com/owl.png"
     end
   end
