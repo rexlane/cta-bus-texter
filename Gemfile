5b4fd742e1c7382fbdd5beb022f5a15b7f1b1211
Rex Lane <rex.m.lane@gmail.com>
2015-12-09 20:27:58 -0600

    Sorted out env variables

--- Gemfile
+++ Gemfile
@@ -3,3 +3,6 @@ source 'https://rubygems.org'
 ruby "2.2.1"
 
 gem 'sinatra'
+gem 'twilio-ruby'
+gem 'geocoder'
+gem 'dotenv'
--- cta-bus-texter.rb
+++ cta-bus-texter.rb
@@ -1,6 +1,30 @@
-# require 'rubygems'
+require 'rubygems'
+require 'twilio-ruby'
 require 'sinatra'
+require 'dotenv'
+require 'geocoder'
+
+Dotenv.load
+
+account_sid = ENV['ACCOUNT_SID']
+auth_token = ENV['AUTH_TOKEN']
+client = Twilio::REST::Client.new account_sid, auth_token
+
+
+@body = params[:Body]
+
 
 get '/' do
-  "Sinatra is running on Heroku"  
+#   'Sinatra is running on Heroku. Currently running version ' + Twilio::VERSION + \
+#         ' of the twilio-ruby library.'
+# end
+
+# get '/sms-quickstart' do
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message do |message| 
+      message.Body "The thing you said was #{@body}"
+      # message.Media "https://demo.twilio.com/owl.png"
+    end
+  end
+  twiml.text
 end
